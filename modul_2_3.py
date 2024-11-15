d71ac648e6011a8a03df5cf2e9eb189487b3eaef
paioneer64 <om3095019@gmail.com>
2024-11-15 19:51:34 +0300

    Add files via upload

---
+++ modul_2_3.py
@@ -0,0 +1,18 @@
+my_list = [42,69,322,13,0,99,-5,9,8,7,-6,5]
+my_list.pop(4)
+index = 0
+
+while index < len(my_list) and my_list[index] > 0:
+    print(my_list[index])
+    index += 1
+    
+
+
+
+
+
+
+
+
+
+
